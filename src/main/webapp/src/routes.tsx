b7de69e763d9d97c6dfa74820d30be46ab3024bd
cesar <cruzatadelacruzc@gmail.com>
2021-09-03 16:50:44 -0400

    Fixed route to LoginRedirect

--- src/main/webapp/src/routes.tsx
+++ src/main/webapp/src/routes.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { useLocation, Switch, RouteComponentProps } from 'react-router-dom';
+import {RouteComponentProps, Switch, useLocation} from 'react-router-dom';
 import ErrorBoundaryRoute from './components/shared/error/error-boundary-route';
 import PageNotFound from './components/shared/error/page-not-found';
 import Directory from './components/module/directory/directory';
 import {AUTHORITIES, CONFIG} from './config/constants';
 import PrivateRoute from './components/shared/auth/private-route';
-import Home from './components/module/home/home';
+import Dashboard from './components/module/dashboard';
 import LoginRedirect from './components/module/login/login-redirect';
 import {Logout} from "./components/module/login/Logout";
 
+/**
+ *The LoginRedirect component always comes before the Dashboard component.
+ * The order is very important, because the PrivateRoute component creates an infinite loop with the path "/"
+ * of the Dashboard component and never looks at the path of the LoginRedirect component. You can define
+ * the most specific Dashboard path as "/dashboard"
+ * @constructor
+ */
 export default function AppRoute() {
   let location = useLocation<RouteComponentProps>();
   React.useEffect(() => window.scrollTo(0, 0), [location.pathname]);
@@ -16,8 +23,9 @@ export default function AppRoute() {
     <Switch>
       <ErrorBoundaryRoute path='/logout' component={Logout} />
       <ErrorBoundaryRoute path='/' exact component={Directory} />
-      <PrivateRoute path='/dashboard' hasAnyAuthorities={[AUTHORITIES.ADMIN]} component={Home} />
+        {/*Read the explanation above the component*/}
       <ErrorBoundaryRoute path={CONFIG.LOGIN_URL} component={LoginRedirect} />
+      <PrivateRoute path='/' hasAnyAuthorities={[AUTHORITIES.ADMIN]} component={Dashboard} />
       <ErrorBoundaryRoute component={PageNotFound} />
     </Switch>
   );
