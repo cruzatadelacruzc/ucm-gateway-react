5602a71992133bec0a98ee5d7701fc95ea02ccbd
cesar <cruzatadelacruzc@gmail.com>
2021-09-04 15:22:50 -0400

    Update index app

--- src/main/webapp/src/index.tsx
+++ src/main/webapp/src/index.tsx
@@ -1,68 +1,23 @@
-import ReactDOM from 'react-dom';
-import React from 'react'
-import './index.css';
 import App from './App';
-import '@fontsource/roboto';
-import reportWebVitals from './reportWebVitals';
-import ErrorBoundary from './components/shared/error/error-boudary';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import { blue, indigo, grey } from '@material-ui/core/colors';
-import { Provider } from 'react-redux'
-import configureStore from "./config/store";
+import React from 'react'
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import {bindActionCreators} from "redux";
-import {clearAuthentication} from "./components/shared/reducer/authenticate";
+import configureStore from "./config/store";
+import reportWebVitals from './reportWebVitals';
 import setupAxiosInterceptors from "./config/api-config";
-import { SnackbarProvider } from "notistack"
-import {SnackbarUtilsConfigurator} from "./components/shared/notification-snackbar.util";
+import ErrorBoundary from './components/shared/error/error-boudary';
+import {clearAuthentication} from "./components/shared/reducer/authenticate";
 
 const store = configureStore();
 const action = bindActionCreators({ clearAuthentication }, store.dispatch);
-setupAxiosInterceptors(() => action.clearAuthentication('login.error.unauthorized'));
-
-const theme = createTheme({
-  palette: {
-    background: {
-      default: grey['100'],
-    },
-    secondary: {
-      main: blue[500],
-    },
-    primary: {
-      main: indigo[500],
-    },
-  },
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: ['"Lato"', 'sans-serif'].join(','),
-  },
-  overrides: {
-    MuiButton: {
-      root: {
-        textTransform: 'none',
-      },
-    },
-    MuiTab: {
-      root: {
-        textTransform: 'none',
-      },
-    },
-  },
-});
+setupAxiosInterceptors(() => action.clearAuthentication('error:login.unauthorized'));
 
 ReactDOM.render(
   <ErrorBoundary>
-    <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <SnackbarProvider maxSnack={3}
-                          preventDuplicate={true}
-                          autoHideDuration={6000}
-                          anchorOrigin={{ vertical: 'bottom', horizontal: 'right'}}
-        >
-          <SnackbarUtilsConfigurator />
         <App />
-        </SnackbarProvider>
       </Provider>
-    </ThemeProvider>
   </ErrorBoundary>,
   document.getElementById('root')
 );
