377e2d22a3f106fdd8477a8b2005e0e395d75b43
cesar <cruzatadelacruzc@gmail.com>
2021-10-15 17:38:19 -0400

    Fixed sessionHasBeenFetched

--- src/main/webapp/src/components/shared/auth/private-route.tsx
+++ src/main/webapp/src/components/shared/auth/private-route.tsx
@@ -1,64 +1,67 @@
 import React from 'react';
 import ErrorBoundary from '../error/error-boudary';
-import { Route, Redirect } from 'react-router-dom';
-import { RouteProps } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
+import {Link, Redirect, Route, RouteProps} from 'react-router-dom';
+import {Button, Typography} from '@material-ui/core';
 import {CONFIG} from "../../../config/constants";
 import {useSelector} from "react-redux";
 import {IRootState} from "../reducer";
+import {useTranslation} from "react-i18next";
 
 interface IPrivateRouteProps extends RouteProps {
-  hasAnyAuthorities?: string[];
+    hasAnyAuthorities: string[];
 }
 
-const PrivateRoute = ({ component: Component, hasAnyAuthorities = [], ...rest }: IPrivateRouteProps) => {
-  const {isAuthenticated, sessionHasBeenFetched, account } = useSelector((states: IRootState) => states.auth);
+const PrivateRoute = ({component: Component, hasAnyAuthorities = [], ...rest}: IPrivateRouteProps) => {
+    const {t} = useTranslation()
+    const account = useSelector((states: IRootState) => states.auth.account);
+    const isAuthenticated = useSelector((states: IRootState) => states.auth.isAuthenticated);
+    const sessionHasBeenFetched = useSelector((states: IRootState) => states.auth.sessionHasBeenFetched);
 
-  if (!Component) throw new Error(`A component needs to be specified for private route for path ${(rest as any).path}`);
+    if (!Component) throw new Error(`A component needs to be specified for private route for path ${(rest as any).path}`);
 
-  const checkAuthorities = props =>
-    hasAnyAuthority(account.authorities, hasAnyAuthorities) ? (
-      <ErrorBoundary>
-        <Component {...props} />
-      </ErrorBoundary>
-    ) : (
-      <div className='insufficient-authority'>
-        <div className='alert alert-danger'>
-          <Typography variant='h4'>You are not authorized to access this page.</Typography>
-        </div>
-      </div>
-    );
+    const checkAuthorities = props =>
+        hasAnyAuthority(account.authorities, hasAnyAuthorities) ? (
+            <ErrorBoundary>
+                <Component {...props} />
+            </ErrorBoundary>
+        ) : (<Typography variant='h4'>{t("common:unauthorized_page")}</Typography>);
 
-  const renderRedirect = props => {
-      if (!sessionHasBeenFetched) {
-          return <div></div>;
-      }
-      else {
-          return isAuthenticated ? (
-              checkAuthorities(props)
-          ) : (
-              <Redirect
-                  to={{
-                      pathname: CONFIG.LOGIN_URL,
-                      search: props.location.search,
-                      state: {from: props.location},
-                  }}
-              />
-          );
-      }
-  };
+    const renderRedirect = props => {
+        if (!sessionHasBeenFetched) {
+            return (
+                <>
+                    {`sessionHasBeenFetched ${sessionHasBeenFetched}`}
+                    <Button component={Link} to="/" color="primary">
+                        Back to Directory
+                    </Button>
+                </>
+            )
+        } else {
+            return isAuthenticated ? (
+                checkAuthorities(props)
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: CONFIG.LOGIN_URL,
+                        search: props.location.search,
+                        state: {from: props.location},
+                    }}
+                />
+            );
+        }
+    };
 
-  return <Route {...rest} render={renderRedirect} />;
+    return <Route {...rest} render={renderRedirect}/>;
 };
 
 export const hasAnyAuthority = (authorities: string[], hasAnyAuthorities: string[]) => {
-  if (authorities && authorities.length !== 0) {
-    if (hasAnyAuthorities.length === 0) {
-      return true;
+    if (authorities && authorities.length !== 0) {
+        if (hasAnyAuthorities.length === 0) {
+            return true;
+        }
+        return hasAnyAuthorities.some(auth => authorities.includes(auth));
     }
-    return hasAnyAuthorities.some(auth => authorities.includes(auth));
-  }
-  return false;
+    return false;
 };
 
 export default PrivateRoute;
