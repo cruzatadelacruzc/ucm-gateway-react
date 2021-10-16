00f12ce332906fa9882fb36646196be6b73e8c09
cesar <cruzatadelacruzc@gmail.com>
2021-10-16 17:19:37 -0400

    Added attributes

--- src/main/webapp/src/components/shared/models/person.model.ts
+++ src/main/webapp/src/components/shared/models/person.model.ts
@@ -1,19 +1,36 @@
+import dayjs from "dayjs";
+
 export interface IPerson {
-  id?: string;
-  active: boolean;
-  description?: string;
-  email?: string;
-  employees?: Array<string>;
-  name: string;
-  phones?: Array<string>;
+    id?: string;
+    ci?: string;
+    name?: string;
+    avatarUrl?: string,
+    email?: string | null;
+    address?: string;
+    firstLastName?: string;
+    secondLastName?: string;
+    gender?: string;
+    age?: number;
+    birthdate?: string | null
+    race?: string;
+    districtId?: string;
+    specialtyId?: string,
+    districtName?: string,
+    specialtyName?: string,
 }
 
 export const defaultValue: Readonly<IPerson> = {
-  id: '',
-  name: '',
-  email: '',
-  active: true,
-  description: '',
-  employees: [],
-  phones: [],
+    ci: '',
+    name: '',
+    email: '',
+    address: '',
+    avatarUrl: '',
+    firstLastName: '',
+    secondLastName: '',
+    gender: '',
+    age: 15,
+    birthdate: dayjs().subtract(15, "year").toISOString(),
+    race: '',
+    districtId: '',
+    specialtyId: ''
 };
