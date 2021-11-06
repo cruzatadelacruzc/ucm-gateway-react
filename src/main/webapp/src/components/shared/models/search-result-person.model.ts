adee3e6b3d78106f2e0e78494988757088f26013
cesar <cruzatadelacruzc@gmail.com>
2021-11-06 16:22:56 -0400

    Modified search person model index

--- src/main/webapp/src/components/shared/models/search-result-person.model.ts
+++ src/main/webapp/src/components/shared/models/search-result-person.model.ts
@@ -1,81 +1,93 @@
-import { ISearchWorkPLaceDetails, defaultWorkPLaceDetailsValue } from './search-result-workplace.model';
+import {defaultDetailsValue as workplaceDetails, ISearchResultWorkPlaceDetails} from './search-result-workplace.model';
 
-export interface ISearchPersonDetails {
+export interface ISearchResultPersonDetails {
+  id: string;
+  profession?: string;
+  secondLastName: string;
+  specialty?: string;
+  address: string;
+  birthdate: string;
+  charge?: string;
+  gender: string;
+  race: string;
+  ci: string;
+  bossWorkPlace: boolean;
+  parentDistrict?: string;
+  firstLastName: string;
+  district?: string;
+  name: string;
+  professionalNumber?: string;
+  registerNumber?: string;
+  category?: string;
+  email: string;
+  age: number;
+  workPlace?: ISearchResultWorkPlaceDetails
+  classRoom?: string;
+  universityYear?: number;
+  residence?: string;
+  kind?: string;
+  studyCenter?: string;
+}
+
+export interface ISearchResultPersonHit {
   _type: string;
   _score: number;
-  _source: {
-    id: string;
-    profession?: string;
-    secondLastName: string;
-    specialty?: string;
-    address: string;
-    birthdate: string;
-    charge?: string;
-    gender: string;
-    race: string;
-    ci: string;
-    bossWorkPlace: boolean;
-    parentDistrict?: string;
-    firstLastName: string;
-    district?: string;
-    name: string;
-    professionalNumber?: string;
-    registerNumber?: string;
-    category?: string;
-    email: string;
-    age: number;
-    workPlace?: ISearchWorkPLaceDetails;
-    classRoom?: string;
-    universityYear?: number;
-    residence?: string;
-    kind?: string;
-    studyCenter?: string;
-  };
+  _source: ISearchResultPersonDetails
+}
+
+export interface ISearchResultPersonHits {
+  total: number;
+  max_score: number;
+  hits: Array<ISearchResultPersonHit>;
 }
 
 export interface ISearchResultPerson {
-  took: number;
-  total: number;
-  max_score: number;
-  hits: Array<ISearchPersonDetails>;
+    took: number;
+    hits: ISearchResultPersonHits;
 }
 
-export const defaultValue: Readonly<ISearchResultPerson> = {
-  took: 0,
-  total: 0,
-  max_score: 0,
-  hits: [
-    {
+export const defaultDetailsValue: Readonly<ISearchResultPersonDetails> = {
+      id: '',
+      profession: '',
+      secondLastName: '',
+      specialty: '',
+      address: '',
+      birthdate: '',
+      charge: '',
+      gender: '',
+      race: '',
+      ci: '',
+      bossWorkPlace: false,
+      parentDistrict: '',
+      firstLastName: '',
+      district: '',
+      name: '',
+      professionalNumber: '',
+      registerNumber: '',
+      category: '',
+      email: '',
+      age: 0,
+      workPlace: workplaceDetails,
+      classRoom: '',
+      universityYear: 1,
+      residence: '',
+      kind: '',
+      studyCenter: '',
+    }
+
+export const defaultHitValue: Readonly<ISearchResultPersonHit> =  {
       _type: '',
       _score: 0,
-      _source: {
-        id: '',
-        profession: '',
-        secondLastName: '',
-        specialty: '',
-        address: '',
-        birthdate: '',
-        charge: '',
-        gender: '',
-        race: '',
-        ci: '',
-        bossWorkPlace: false,
-        parentDistrict: '',
-        firstLastName: '',
-        district: '',
-        name: '',
-        professionalNumber: '',
-        registerNumber: '',
-        category: '',
-        email: '',
-        age: 0,
-        workPlace: defaultWorkPLaceDetailsValue,
-        classRoom: '',
-        universityYear: 1,
-        residence: '',
-        kind: '',
-        studyCenter: '',
-      },
-    },
-  ],
+      _source: defaultDetailsValue
+    }
+
+export const defaultHitsValue: Readonly<ISearchResultPersonHits> = {
+  total: 0,
+  max_score: 0,
+  hits: [defaultHitValue],
 };
+
+export const defaultValue: Readonly<ISearchResultPerson> = {
+    took: 0,
+    hits: defaultHitsValue
+}
