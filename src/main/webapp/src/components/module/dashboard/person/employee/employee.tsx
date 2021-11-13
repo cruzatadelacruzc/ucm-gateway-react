8d09e626082bbf3287afa6bcf973bf0647ec57a4
cesar <cruzatadelacruzc@gmail.com>
2021-11-13 17:58:35 -0500

    Added redirect show Workplace button

--- src/main/webapp/src/components/module/dashboard/person/employee/employee.tsx
+++ src/main/webapp/src/components/module/dashboard/person/employee/employee.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {useTranslation} from "react-i18next";
 import UCMDataBase from "../../../../shared/components/datatable";
 import {ACTION_TYPES} from "./employee.reducer";
+import {Button} from "@material-ui/core";
+import {Link} from "react-router-dom";
 
 const Employees = () => {
     const {t} = useTranslation(['employee']);
@@ -56,8 +58,26 @@ const Employees = () => {
         },
         {
             name: 'workPlaceName',
-            label: t("workPlace")
-        }
+            label: t("workPlace"),
+            options: {
+                customBodyRender: (value, tableMeta) => {
+                    const _workplaceId = tableMeta.rowData?.length > 0 && tableMeta.rowData[9]
+                    return _workplaceId ?
+                        <Button variant="text" component={Link} to={`/workplace/show/${_workplaceId}`}>{value}</Button> :
+                        ''
+                }
+            }
+        },
+        {
+            name: "workPlaceId",
+            options: {
+                display: 'excluded',
+                print: false,
+                filter: false,
+                download: false,
+                searchable: false
+            }
+        },
     ];
 
     return (
