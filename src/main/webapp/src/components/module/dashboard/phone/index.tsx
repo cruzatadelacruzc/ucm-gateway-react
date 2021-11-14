6492bfedff197d5b812711e1781ca606b09a0d32
cesar <cruzatadelacruzc@gmail.com>
2021-11-13 22:31:38 -0500

    Added link to update phone

--- src/main/webapp/src/components/module/dashboard/phone/index.tsx
+++ src/main/webapp/src/components/module/dashboard/phone/index.tsx
@@ -7,6 +7,7 @@ import ErrorBoundaryRoute from "../../../shared/error/error-boundary-route";
 export default function Routes({match}: RouteComponentProps) {
     return (
         <Switch>
+            <ErrorBoundaryRoute exact path={`${match.url}/edit/:id`} component={PhoneManage}/>
             <ErrorBoundaryRoute exact path={`${match.url}/add`} component={PhoneManage}/>
             <ErrorBoundaryRoute path={match.url} component={Phones}/>
         </Switch>
