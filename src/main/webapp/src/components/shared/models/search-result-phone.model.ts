8ade731812c57c0126dbe61aefbeaf03ec5d53e5
cesar <cruzatadelacruzc@gmail.com>
2021-12-15 15:14:17 -0500

    Modified model search phone

--- src/main/webapp/src/components/shared/models/search-result-phone.model.ts
+++ src/main/webapp/src/components/shared/models/search-result-phone.model.ts
@@ -1,36 +1,52 @@
-import { ISearchResultPerson, defaultValue as SearchResultPerson } from './search-result-person.model';
-import { ISearchResultWorkPlace, defaultValue as SearchResultPlace } from './search-result-workplace.model';
+import {defaultDetailsValue as personDetails, ISearchResultPersonDetails} from './search-result-person.model';
+import {defaultDetailsValue as workPlaceDetails, ISearchResultWorkPlaceDetails,} from './search-result-workplace.model';
 
-export interface ISearchResultPhone {
-  total: number;
-  hits: [
-    {
-      _type: string;
-      _score: number;
-      _source: {
-        id: string;
-        number: number;
-        description?: string;
-        employee?: ISearchResultPerson;
-        workPlace?: ISearchResultWorkPlace;
-      };
-    }
-  ];
+export interface ISearchResultPhoneDetails {
+  id: string;
+  number: number;
+  description?: string;
+  employee?: ISearchResultPersonDetails;
+  workPlace?: ISearchResultWorkPlaceDetails;
 }
 
-export const defaultValue: Readonly<ISearchResultPhone> = {
-  total: 0,
-  hits: [
-    {
+export interface ISearchResultPhoneHit {
+  _type: string;
+  _score: number;
+  _source: ISearchResultPhoneDetails
+}
+
+export interface ISearchResultPhoneHits {
+  total: number;
+  max_score: number;
+  hits: Array<ISearchResultPhoneHit>
+}
+
+export interface ISearchResultPhone {
+    took: number;
+    hits: ISearchResultPhoneHits
+}
+
+export const defaultDetailsValue: Readonly<ISearchResultPhoneDetails> = {
+      id: '',
+      number: 0,
+      description: '',
+      employee: personDetails,
+      workPlace: workPlaceDetails,
+    }
+
+export const defaultHitValue: Readonly<ISearchResultPhoneHit> =    {
       _type: '',
       _score: 0,
-      _source: {
-        id: '',
-        number: 0,
-        description: '',
-        employee: SearchResultPerson,
-        workPlace: SearchResultPlace,
-      },
-    },
-  ],
+      _source: defaultDetailsValue
+    }
+
+export const defaultHitsValue: Readonly<ISearchResultPhoneHits> = {
+  total: 0,
+  max_score: 0,
+  hits: [ defaultHitValue],
 };
+
+export const defaultValue: Readonly<ISearchResultPhone> = {
+    took: 0,
+    hits: defaultHitsValue
+}
