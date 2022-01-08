619c35b93c6b5f2c3e318b451e18843dc1b8bacd
cesar <cruzatadelacruzc@gmail.com>
2022-01-08 11:41:44 -0500

    Modified person view

--- src/main/webapp/src/components/module/dashboard/person/index.tsx
+++ src/main/webapp/src/components/module/dashboard/person/index.tsx
@@ -8,7 +8,7 @@ import {Field} from 'formik';
 import {getDistricts, getSpecialties} from "../nomenclature/nomenclature.reducer";
 import {DatePicker} from "formik-material-ui-pickers";
 import {TextField} from 'formik-material-ui';
-import {Box, MenuItem} from "@material-ui/core";
+import {Box, Grid, MenuItem} from "@material-ui/core";
 import {formUpdateStyles, MenuProps} from "../style";
 import {MuiPickersUtilsProvider} from "@material-ui/pickers";
 import DayjsUtils from "@date-io/dayjs";
@@ -36,7 +36,7 @@ export const _validationSchema = yup.object().shape({
 interface IPersonStep {
     isNew: boolean,
     person: IEmployee | IStudent,
-    setFileInput: React.Dispatch<React.SetStateAction<File| undefined>>
+    setFileInput: React.Dispatch<React.SetStateAction<File | undefined>>
 }
 
 const PersonalStep = React.memo(({isNew, person, setFileInput}: IPersonStep) => {
@@ -55,129 +55,163 @@ const PersonalStep = React.memo(({isNew, person, setFileInput}: IPersonStep) => 
 
     return (
         <MuiPickersUtilsProvider utils={DayjsUtils}>
-                <Box className={classes.form_group}>
-                    <Box className={classes.form_group}>
+            <Grid container>
+                <Grid container item md={4} sm={12} xs={12} lg={4} justifyContent="center">
+                        <UCMAvatar
+                            avatarUrl={person.avatarUrl}
+                            setResultAvatar={setFileInput}
+                            deleteAvatar={() => dispatch(deleteAvatar(person.id))}
+                        />
+                </Grid>
+                <Grid container item md={8} sm={12} xs={12} lg={8}>
+                    <Grid item md={6} sm={6} xs={12} lg={6}>
                         <Box className={classes.input}>
-                            <UCMAvatar
-                                avatarUrl={person.avatarUrl}
-                                setResultAvatar={setFileInput}
-                                deleteAvatar={() => dispatch(deleteAvatar(person.id))}
+                            <Field name="ci" label={t('ci')} variant="outlined" fullWidth
+                                   InputLabelProps={{shrink: true}}
+                                   component={TextField}/>
+                        </Box>
+                    </Grid>
+                    <Grid item md={12} sm={6} xs={12} lg={12}>
+                        <Box className={classes.input}>
+                            <Field name="email" label={t('email')} variant="outlined" fullWidth
+                                   InputLabelProps={{shrink: true}}
+                                   component={TextField}/>
+                        </Box>
+                    </Grid>
+                    <Grid item md={12} sm={12} xs={12} lg={12}>
+                        <Box className={classes.input}>
+                            <Field name="address" label={t('address')}
+                                   variant="outlined"
+                                   fullWidth
+                                   multiline
+                                   inputProps={{maxLength: "255"}}
+                                   InputLabelProps={{shrink: true}}
+                                   component={TextField}
                             />
                         </Box>
-                    </Box>
-                    <Box className={classes.inputSM}>
-                        <Field name="ci" label={t('ci')} variant="outlined" fullWidth InputLabelProps={{shrink: true}}
-                               component={TextField}/>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field name="email" label={t('email')} variant="outlined" fullWidth
-                               InputLabelProps={{shrink: true}}
-                               component={TextField}/>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field name="address" label={t('address')} variant="outlined" fullWidth
-                               InputLabelProps={{shrink: true}}
-                               component={TextField}/>
-                    </Box>
+                    </Grid>
+                </Grid>
+            <Grid item container md={12} sm={12} xs={12} lg={12}>
+                <Box className={classes.input}>
+                    <Field name="name" label={t('name')}
+                           variant="outlined"
+                           fullWidth
+                           multiline
+                           InputLabelProps={{shrink: true}}
+                           inputProps={{maxLength: "255"}}
+                           component={TextField}/>
                 </Box>
-                <Box className={classes.form_group}>
-                    <Box className={classes.input}>
-                        <Field name="name" label={t('name')} variant="outlined" fullWidth
-                               InputLabelProps={{shrink: true}}
-                               component={TextField}/>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field name="firstLastName" label={t('firstLastName')} fullWidth variant="outlined"
-                               InputLabelProps={{shrink: true}} component={TextField}/>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field name="secondLastName" label={t('secondLastName')} fullWidth variant="outlined"
-                               InputLabelProps={{shrink: true}} component={TextField}/>
-                    </Box>
+
+                <Box className={classes.input}>
+                    <Field name="firstLastName" label={t('firstLastName')}
+                           fullWidth
+                           multiline
+                           variant="outlined"
+                           component={TextField}
+                           InputLabelProps={{shrink: true}}
+                           inputProps={{maxLength: "255"}}
+                    />
                 </Box>
-                <Box className={classes.form_group}>
-                    <Box className={classes.input}>
-                        <Field
-                            select
-                            fullWidth
-                            name="gender"
-                            variant="outlined"
-                            component={TextField}
-                            SelectProps={MenuProps}
-                            label={t('gender.label')}
-                            InputLabelProps={{shrink: true}}
-                        >
-                            <MenuItem value=""><em>-- {t('common:empty')} --</em></MenuItem>
-                            <MenuItem value={PERSON_GENDER.FEMALE}
-                                      selected={!isNew && person.gender !== undefined && person.gender === PERSON_GENDER.FEMALE}>
-                                {t("gender.female")}
+
+                <Box className={classes.input}>
+                    <Field name="secondLastName" label={t('secondLastName')}
+                           fullWidth
+                           variant="outlined"
+                           component={TextField}
+                           InputLabelProps={{shrink: true}}
+                           inputProps={{maxLength: "255"}}
+                    />
+                </Box>
+            </Grid>
+            <Grid item container md={12} sm={12} xs={12} lg={12}>
+                <Box className={classes.input}>
+                    <Field
+                        select
+                        fullWidth
+                        name="gender"
+                        variant="outlined"
+                        component={TextField}
+                        SelectProps={MenuProps}
+                        label={t('gender.label')}
+                        InputLabelProps={{shrink: true}}
+                    >
+                        <MenuItem value=""><em>-- {t('common:empty')} --</em></MenuItem>
+                        <MenuItem value={PERSON_GENDER.FEMALE}
+                                  selected={!isNew && person.gender !== undefined && person.gender === PERSON_GENDER.FEMALE}>
+                            {t("gender.female")}
+                        </MenuItem>
+                        <MenuItem value={PERSON_GENDER.MALE}
+                                  selected={!isNew && person.gender !== undefined && person.gender === PERSON_GENDER.MALE}>
+                            {t("gender.male")}
+                        </MenuItem>
+                    </Field>
+                </Box>
+
+
+                <Box className={classes.input}>
+                    <Field name="race" label={t('race')} fullWidth InputLabelProps={{shrink: true}}
+                           variant="outlined" component={TextField}/>
+                </Box>
+
+
+                <Box className={classes.input}>
+                    <Field
+                        autoOk
+                        fullWidth
+                        inputVariant="outlined"
+                        name={"birthdate"}
+                        component={DatePicker}
+                        label={t('birthdate')}
+                        InputLabelProps={{shrink: true}}
+                        format={t("common:date_format")}
+                    />
+                </Box>
+
+            </Grid>
+            <Grid item container>
+                <Box className={classes.input}>
+                    <Field
+                        select
+                        fullWidth
+                        name="districtId"
+                        variant="outlined"
+                        component={TextField}
+                        SelectProps={MenuProps}
+                        label={t('district')}
+                        InputLabelProps={{shrink: true}}
+                    >
+                        <MenuItem value=""><em>-- {t('common:empty')} --</em></MenuItem>
+                        {districts.map((option, index) => (
+                            <MenuItem key={index} value={option.id}
+                                      selected={!isNew && person.districtId !== undefined && person.districtId === option.id}>
+                                {option.name}
                             </MenuItem>
-                            <MenuItem value={PERSON_GENDER.MALE}
-                                      selected={!isNew && person.gender !== undefined && person.gender === PERSON_GENDER.MALE}>
-                                {t("gender.male")}
+                        ))}
+                    </Field>
+                </Box>
+                <Box className={classes.input}>
+                    <Field
+                        select
+                        fullWidth
+                        name="specialtyId"
+                        variant="outlined"
+                        component={TextField}
+                        SelectProps={MenuProps}
+                        label={t('specialty')}
+                        InputLabelProps={{shrink: true}}
+                    >
+                        <MenuItem value=""><em>-- {t('common:empty')} --</em></MenuItem>
+                        {specialties.map((option, index) => (
+                            <MenuItem key={index} value={option.id}
+                                      selected={!isNew && person.specialtyId !== undefined && person.specialtyId === option.id}>
+                                {option.name}
                             </MenuItem>
-                        </Field>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field name="race" label={t('race')} fullWidth InputLabelProps={{shrink: true}}
-                               variant="outlined" component={TextField}/>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field
-                            autoOk
-                            fullWidth
-                            inputVariant="outlined"
-                            name={"birthdate"}
-                            component={DatePicker}
-                            label={t('birthdate')}
-                            InputLabelProps={{shrink: true}}
-                            format={t("common:date_format")}
-                        />
-                    </Box>
+                        ))}
+                    </Field>
                 </Box>
-                <Box className={classes.form_group}>
-                    <Box className={classes.input}>
-                        <Field
-                            select
-                            fullWidth
-                            name="districtId"
-                            variant="outlined"
-                            component={TextField}
-                            SelectProps={MenuProps}
-                            label={t('district')}
-                            InputLabelProps={{shrink: true}}
-                        >
-                            <MenuItem value=""><em>-- {t('common:empty')} --</em></MenuItem>
-                            {districts.map((option, index) => (
-                                <MenuItem key={index} value={option.id}
-                                          selected={!isNew && person.districtId !== undefined && person.districtId === option.id}>
-                                    {option.name}
-                                </MenuItem>
-                            ))}
-                        </Field>
-                    </Box>
-                    <Box className={classes.input}>
-                        <Field
-                            select
-                            fullWidth
-                            name="specialtyId"
-                            variant="outlined"
-                            component={TextField}
-                            SelectProps={MenuProps}
-                            label={t('specialty')}
-                            InputLabelProps={{shrink: true}}
-                        >
-                            <MenuItem value=""><em>-- {t('common:empty')} --</em></MenuItem>
-                            {specialties.map((option, index) => (
-                                <MenuItem key={index} value={option.id}
-                                          selected={!isNew && person.specialtyId !== undefined && person.specialtyId === option.id}>
-                                    {option.name}
-                                </MenuItem>
-                            ))}
-                        </Field>
-                    </Box>
-                </Box>
-            </MuiPickersUtilsProvider>
+            </Grid>
+            </Grid>
+        </MuiPickersUtilsProvider>
     )
 })
 
