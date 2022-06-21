2ede5e178ffaac9c2295d3320d6b2112f29bcf99
cesar <cruzatadelacruzc@gmail.com>
2022-06-21 18:09:33 -0400

    Added develop profile

--- src/main/java/sld/ucm/gateway/config/Constants.java
+++ src/main/java/sld/ucm/gateway/config/Constants.java
@@ -5,6 +5,7 @@ package sld.ucm.gateway.config;
  */
 public final class Constants {
     public static final String PROFILE_DEV = "dev";
+    public static final String PROFILE_UCM = "ucm";
     public static final String PROFILE_PROD = "prod";
     public static final String DEFAULT_LANGUAGE = "en";
 }
