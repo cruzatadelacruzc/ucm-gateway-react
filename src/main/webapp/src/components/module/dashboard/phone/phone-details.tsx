a0cd16dc1fb4129dccc8eb95d5400be79e28755b
cesar <cruzatadelacruzc@gmail.com>
2022-08-08 07:03:13 -0400

    Updated phone details

--- src/main/webapp/src/components/module/dashboard/phone/phone-details.tsx
+++ src/main/webapp/src/components/module/dashboard/phone/phone-details.tsx
@@ -1,49 +1,53 @@
 import React from 'react';
 import Widget from "../../../shared/layout/widget";
 import {useDispatch, useSelector} from "react-redux";
-import {detailsStyles} from "../style";
 import {deletePhone, getPhone} from "./phone.reducer";
 import {Link, useNavigate, useParams} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {IRootState} from "../../../shared/reducer";
 import HomeWorkIcon from '@mui/icons-material/HomeWork';
-import {Avatar, Box, Button, Chip, CircularProgress, Divider, Stack, styled, Typography, useTheme} from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Button,
+    Chip,
+    CircularProgress,
+    Divider,
+    FormControl,
+    FormLabel,
+    Grid,
+    IconButton,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Typography,
+    useTheme
+} from "@mui/material";
 import DialogDelete from "../../../shared/components/dialog-delete";
 import {buildAvatarURL} from "../../../shared/util/function-utils";
 import CancelIcon from "@mui/icons-material/Cancel";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-const Item = styled("div")(({theme}) => ({
-    marginBottom: theme.spacing(1),
-    [theme.breakpoints.down('sm')]: {
-        flexDirection: "column",
-    },
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    display: "flex"
-}));
-
-const Root = styled("div")(({theme}) => ({
-    width: '100%',
-    ...theme.typography.body2,
-    '& > :not(style) + :not(style)': {
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(2),
-    }
-}));
-
 const PhoneDetails = () => {
     const theme = useTheme();
     let navigate = useNavigate();
     const dispatch = useDispatch();
-    const classes = detailsStyles();
     let {id} = useParams<{ id: string }>();
     const {t} = useTranslation(['phone']);
     const [modalOpen, setModalOpen] = React.useState(false);
     const _entity = useSelector((states: IRootState) => states.phone.entity);
     const updating = useSelector((states: IRootState) => states.phone.updating);
     const isUpdateSuccess = useSelector((states: IRootState) => states.phone.updateSuccess);
+
+    const buttonSX = {
+        marginRight: 3,
+        [theme.breakpoints.down('sm')]: {
+            width: "100%",
+            marginRight: 0,
+            marginBottom: 1
+        }
+    }
 
     React.useEffect(() => {
         if (undefined !== id) {
@@ -61,85 +65,96 @@ const PhoneDetails = () => {
 
     return (
         <Widget disableWidgetMenu>
-            <Root>
+            <Box sx={{
+                width: '100%',
+                ...theme.typography.body2,
+                '& > :not(style) + :not(style)': {
+                    marginTop: 2,
+                    marginBottom: 2,
+                }
+            }}>
                 <Box sx={{display: 'flex'}}>
                     <Divider sx={{order: 1, flex: '1 1 auto'}}/>
                     <Chip variant='outlined' label={t('detail.title')} sx={{order: 2, flex: '1 1 auto'}}/>
                     <Divider sx={{order: 3, flex: '1 1 auto'}}/>
                 </Box>
-                <Stack direction="column" justifyContent="space-evenly" alignItems="flex-start">
-                    <Item>
-                        <Typography variant="subtitle1"
-                                    style={{marginRight: theme.spacing(1)}}>{t("active")} :</Typography>
-                        <Typography variant="subtitle1"
-                                    color='primary'>{_entity.active ? t("positive") : "NO"}</Typography>
-                    </Item>
-                    <Item>
-                        <Typography variant="subtitle1"
-                                    style={{marginRight: theme.spacing(1)}}>{t("number")} :</Typography>
-                        <Typography variant="subtitle1">{_entity.number}</Typography>
-                    </Item>
-                    <Item>
-                        <Typography variant="subtitle1"
-                                    style={{marginRight: theme.spacing(1)}}>{t("description")}:</Typography>
-                        <Typography variant="subtitle1">{_entity.description ? _entity.description : '-'}</Typography>
-                    </Item>
-                    {_entity.employee
-                        ?
-                        <Item>
-                            <Avatar
-                                alt={`${_entity.employee?.name} ${_entity.employee?.firstLastName} ${_entity.employee?.secondLastName}`}
-                                src={_entity.employee.avatarUrl ? buildAvatarURL(_entity.employee.avatarUrl) : ""}
-                            />
-                            <Button variant="text" color='primary' component={Link}
-                                    to={`/dashboard/employee/show/${_entity.employee.id}`}>
-                                <Typography variant="subtitle1" style={{borderBottom: '2px dotted red'}}>
-                                    {`${_entity.employee?.name} ${_entity.employee?.firstLastName} ${_entity.employee?.secondLastName}`}
-                                </Typography>
-                            </Button>
-                        </Item>
-                        : ''
-                    }
-                    {_entity.workPlace
-                        ?
-                        <Item>
-                            {_entity.workPlace?.avatarUrl ?
-                                <Avatar alt={_entity.workPlace?.name}
-                                        src={buildAvatarURL(_entity.workPlace.avatarUrl)}/>
-                                :
-                                <HomeWorkIcon fontSize='large' style={{marginRight: theme.spacing(1)}}/>
-                            }
-                            <Button variant="text" color='primary' component={Link} sx={{textTransform: 'uppercase'}}
-                                    to={`/dashboard/workplace/show/${_entity.workPlace?.id}`}>
-                                <Typography variant="subtitle1" style={{borderBottom: '2px dotted red'}}>
-                                    {_entity.workPlace?.name}
-                                </Typography>
-                            </Button>
-                        </Item>
-                        : ''
-                    }
-                    <Box className={classes.buttons}>
+                <Grid container spacing={{xs: 2, md: 1}}>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">{t("active")}</FormLabel>
+                            {_entity.active ? t("positive") : "NO"}
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">{t("number")}</FormLabel>
+                            {_entity.number}
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">{t("description")}</FormLabel>
+                            {_entity.description ? _entity.description : '-'}
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="h3">{t("owner")}</Typography>
+                        {_entity.workPlace &&
+                        <IconButton size="large" sx={{p: 0, borderRadius: 0}}>
+                            <ListItem alignItems="flex-start" component={Link} sx={{pl: 0}}
+                                      to={`/dashboard/workplace/show/${_entity.workPlace?.id}`}>
+                                <ListItemAvatar>
+                                    {_entity.workPlace?.avatarUrl
+                                        ?
+                                        <Avatar alt={_entity.workPlace?.name}
+                                                src={buildAvatarURL(_entity.workPlace.avatarUrl)}
+                                        />
+                                        :
+                                        <HomeWorkIcon fontSize='large' style={{marginRight: theme.spacing(1)}}/>
+                                    }
+                                </ListItemAvatar>
+                                <ListItemText primary={_entity.workPlace.name}/>
+                            </ListItem>
+                        </IconButton>
+                        }
+
+                        {_entity.employee &&
+                        <IconButton size="large" sx={{p: 0, borderRadius: 0}}>
+                            <ListItem alignItems="flex-start" component={Link} sx={{pl: 0}}
+                                      to={`/dashboard/employee/show/${_entity.employee?.id}`}>
+                                <ListItemAvatar>
+                                    <Avatar alt={_entity.employee?.name} sizes="medium"
+                                            src={_entity.employee?.avatarUrl && buildAvatarURL(_entity.employee.avatarUrl)}/>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={`${_entity.employee.name} ${_entity.employee.firstLastName} ${_entity.employee.secondLastName}`}
+                                    secondary={_entity.employee.chargeName}/>
+                            </ListItem>
+                        </IconButton>
+                        }
+                    </Grid>
+                    <Grid item xs={12}>
                         <Button
+                            sx={buttonSX}
                             component={Link}
                             color="secondary"
                             startIcon={<CancelIcon/>}
                             variant="contained"
-                            to={'/dashboard/phone'}
-                            className={classes.button}>
+                            to={'/dashboard/phone'}>
                             {t('common:close')}
                         </Button>
                         <Button
+                            sx={buttonSX}
                             component={Link}
                             color="secondary"
                             startIcon={<EditIcon/>}
                             variant="contained"
-                            className={classes.button}
                             to={`/dashboard/phone/edit/${id}`}>
                             {t('common:edit')}
                         </Button>
                         <Button
+                            sx={buttonSX}
                             variant="contained"
-                            className={classes.button}
                             onClick={() => setModalOpen(true)}
                             disabled={updating}
                             startIcon={updating ? <CircularProgress size="1rem"/> : <DeleteIcon/>}
@@ -152,9 +167,9 @@ const PhoneDetails = () => {
                             title={t("delete.title")}
                             deleteItem={() => dispatch(deletePhone(id))}
                             content={t("delete.question", {param: _entity.number})}/>
-                    </Box>
-                </Stack>
-            </Root>
+                    </Grid>
+                </Grid>
+            </Box>
         </Widget>
     );
 };
