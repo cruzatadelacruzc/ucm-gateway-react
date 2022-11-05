ee240b131f7733678b0e7efd2d88f6010925ebfe
cesar <cruzatadelacruzc@gmail.com>
2022-11-05 13:34:41 -0400

    Fixed invalid prop container

--- src/main/webapp/src/components/shared/layout/sidebar/index.tsx
+++ src/main/webapp/src/components/shared/layout/sidebar/index.tsx
@@ -16,52 +16,76 @@ const Sidebar = ({toggleSidebar, isSidebarOpened}: ISidebar) => {
     const matchUpMd = useMediaQuery(theme.breakpoints.up('md'));
     const container = window !== undefined ? () => window.document.body : undefined;
 
-   return (
-       <Box component="nav" sx={{flexShrink: { md: 0 }, width: matchUpMd ? drawerWidth : 'auto' }} aria-label="mailbox folders">
-       <Drawer
-           anchor="left"
-           container={container}
-           open={isSidebarOpened}
-           onClose={toggleSidebar}
-           variant={matchUpMd ? 'persistent' : 'temporary'}
-           sx={{
-               '& .MuiDrawer-paper': {
-                   width: drawerWidth,
-                   background: theme.palette.background.default,
-                   color: theme.palette.text.primary,
-                   borderRight: 'none',
-                   [theme.breakpoints.up('md')]: {
-                       top: '88px'
-                   }
-               }
-           }}
-           ModalProps={{ keepMounted: true }}
-           color="inherit"
-       >
-           <Box sx={{display: { xs: 'block', md: 'none' }}}>
-               <Box sx={{ display: 'flex', p: 2, mx: 'auto' }}>
-                   <LogoSection/>
-               </Box>
-           </Box>
-           <BrowserView>
-               <PerfectScrollbar
-                   component="div"
-                   style={{
-                       height: !matchUpMd ? 'calc(100vh - 56px)' : 'calc(100vh - 88px)',
-                       paddingLeft: '16px',
-                       paddingRight: '16px'
+    const drawer = (
+        <>
+            <Box sx={{display: {xs: 'block', md: 'none'}}}>
+                <Box sx={{display: 'flex', p: 2, mx: 'auto'}}>
+                    <LogoSection/>
+                </Box>
+            </Box>
+            <BrowserView>
+                <PerfectScrollbar
+                    component="div"
+                    style={{
+                        height: !matchUpMd ? 'calc(100vh - 56px)' : 'calc(100vh - 88px)',
+                        paddingLeft: '16px',
+                        paddingRight: '16px'
                     }}
-               >
-                <MenuItems/>
-               </PerfectScrollbar>
-           </BrowserView>
-           <MobileView>
-               <Box sx={{px: 2}}>
-                   <MenuItems/>
-               </Box>
-           </MobileView>
-       </Drawer>
-       </Box>
-   );}
+                >
+                    <MenuItems/>
+                </PerfectScrollbar>
+            </BrowserView>
+            <MobileView>
+                <Box sx={{px: 2}}>
+                    <MenuItems/>
+                </Box>
+            </MobileView>
+        </>
+    )
+
+    return (
+        <Box component="nav" sx={{flexShrink: {md: 0}, width: matchUpMd ? drawerWidth : 'auto'}}
+             aria-label="mailbox folders">
+            <Drawer
+                anchor="left"
+                container={container}
+                open={isSidebarOpened}
+                onClose={toggleSidebar}
+                variant={'temporary'}
+                sx={{
+                    display: {xs: 'block', md: 'none'},
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        background: theme.palette.background.default,
+                        color: theme.palette.text.primary,
+                        borderRight: 'none'
+                    }
+                }}
+                ModalProps={{keepMounted: true}}
+            >
+                {drawer}
+            </Drawer>
+            <Drawer
+                anchor="left"
+                container={container}
+                open={isSidebarOpened}
+                onClose={toggleSidebar}
+                variant={'persistent'}
+                sx={{
+                    display: {xs: 'none', md: 'block'},
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        background: theme.palette.background.default,
+                        color: theme.palette.text.primary,
+                        borderRight: 'none',
+                        top: '88px'
+                    }
+                }}
+            >
+                {drawer}
+            </Drawer>
+        </Box>
+    );
+}
 
 export default Sidebar;
